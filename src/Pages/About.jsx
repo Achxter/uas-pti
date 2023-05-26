d900330cfe034cb442e3d27835dcedcf084be78c
Achxter <hansphilemon@gmail.com>
2023-05-26 16:49:11 +0700

    little about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -78,87 +78,63 @@ function About() {
 
   return (
     <div id="background">
-      <div className="container mx-auto mt-8 md:mt-16 ps-4 py-8">
-        <Navbar />
-        <div className="mt-8">
-          <div className='about flex justify-center'>
-            <h1 className="text-3xl font-bold mb-4 text-center-mobile bg-black/[.75] px-4 py-2 rounded-md">
-              About Us
-            </h1>
-          </div>
-          <div className="flex justify-center">
-            <div className="md:grid md:grid-cols-2 md:gap-16 lg:grid-cols-4">
-              {Users.map((user) => (
-                <div key={user.id} className="flip-card mt-4 flip-card-small w-60 h-80 md:w-48 md:h-64 xl:w-60 xl:h-80">
-                  <div className="flip-card-inner">
-                    <div className="flip-card-front fixed relative">
-                      <img src={user.image} alt={user.name} className="user-image" />
-                    </div>
-                    <div className="flip-card-back flex wrap">
-                      <div className='logoWEMEN'>
-                        <div className="umnBulet">
-                          <img src={umnBulet} alt="UMN Circle" />
-                        </div>
-                        <div className='umn'>
-                          <a href="https://www.umn.ac.id/en/home/">
-                            <img src={umn} alt="UMN" />
-                          </a>
-                        </div>
-                      </div>
-                      <div className="user-info mt-4">
-                        {user.nim}
-                        <br />
-                        <span className='untukNama'>{user.name}</span>
-                        <br />
-                        <p>Informatika</p>
-                        <p className='italic'>Agustus 2026</p>
-                        <span className='igNiha'>Instagram: <a href={user.ig} className='igNih'>{user.profile}</a></span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          {/* <div className="flex justify-center">
-            <div className="md:grid md:grid-cols-2 md:gap-16 lg:grid-cols-4">
-              {Users.map(user => (
-                <div key={user.id} className="flip-card mt-4 flip-card-small w-60 h-80 md:w-48 md:h-64 xl:w-60 xl:h-80">
-                  <div className="flip-card-inner">
-                    <div className="flip-card-front fixed">
-                      <img src={user.image} alt={user.name} className="user-image" />
-                      <div className="shine"></div>
-                    </div>
-
-
-                    <div className="flip-card-back flex wrap">
-                      <p className="user-info">
-                        Name: {user.name}
-                        <br />
-                        NIM: {user.nim}
-                        <br />
-                        Instagram: <a href={user.ig}>{user.profile}</a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div> */}
-          <div className="atasin">
+      <Navbar />
+      <div className="container mx-auto mt-8 md:mt-16 px-4 pt-8">
+        <div className='bg-white/[.06] shadow-2xl backdrop-blur rounded-md px-1 py-4 mt-4'>
+          <div className="mt-8">
             <div className='about flex justify-center'>
-              <h1 className="text-3xl font-bold mb-4 text-center-mobile bg-black/[.8] px-4 py-2 rounded">
-                References
+              <h1 className="text-3xl font-bold mb-4 text-center-mobile ">
+                About Us
               </h1>
             </div>
-            <div className='flex flex-col items-center'>
-              {Links.map(link => (
-                <div className="flex items-center bg-slate-100/[.9] p-4 rounded-md justify-center w-1/2 md:w-1/4 my-2" key={link.id}>
-                  <img src="" className="bullet mr-4" alt="" />
-                  <p className="hover:translate-x-1 duration-100 ease-in-out"><a className="font-bold text-black hover:text-blue-500" href={link.fill.props.href}>{link.fill.props.children}</a></p>
-                </div>
-              ))}
+            <div className="flex justify-center mt-12">
+              <div className="md:grid md:grid-cols-2 md:gap-16 lg:grid-cols-4">
+                {Users.map((user) => (
+                  <div key={user.id} className="flip-card mt-4 flip-card-small w-60 h-80 md:w-48 md:h-64 xl:w-60 xl:h-80">
+                    <div className="flip-card-inner">
+                      <div className="flip-card-front fixed relative">
+                        <img src={user.image} alt={user.name} className="user-image" />
+                      </div>
+                      <div className="flip-card-back flex wrap">
+                        <div className='logoWEMEN'>
+                          <div className="umnBulet">
+                            <img src={umnBulet} alt="UMN Circle" />
+                          </div>
+                          <div className='umn'>
+                            <a href="https://www.umn.ac.id/en/home/">
+                              <img src={umn} alt="UMN" />
+                            </a>
+                          </div>
+                        </div>
+                        <div className="user-info mt-4">
+                          {user.nim}
+                          <br />
+                          <span className='untukNama'>{user.name}</span>
+                          <br />
+                          <p>Informatika</p>
+                          <p className='italic'>Agustus 2026</p>
+                          <span className='igNiha'>Instagram: <a href={user.ig} className='igNih'>{user.profile}</a></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="atasin">
+              <div className='about flex justify-center'>
+                <h1 className="text-3xl font-bold mb-4 text-center-mobile">
+                  References
+                </h1>
+              </div>
+              <div className='flex flex-col items-center'>
+                {Links.map(link => (
+                  <div className="flex items-center bg-slate-100/[.9] p-4 rounded-md justify-center w-1/2 md:w-1/4 my-2" key={link.id}>
+                    <img src="" className="bullet mr-4" alt="" />
+                    <p className="hover:translate-x-1 duration-100 ease-in-out"><a className="font-bold text-black hover:text-blue-500" href={link.fill.props.href}>{link.fill.props.children}</a></p>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
