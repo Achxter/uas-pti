e8077650fa90b65db5fe33d9349937c878a4ca78
Achxter <hansphilemon@gmail.com>
2023-06-02 12:14:23 +0700

    finishing search and pokeinfo

--- src/Components/Pokeinfo.jsx
+++ src/Components/Pokeinfo.jsx
@@ -10,13 +10,13 @@ const Pokeinfo = ({ data }) => {
         (!data) ? "" : (
           <div id="pokeCard" className='m-4 p-4 !border-0 bg-white rounded-md'>
             <div className='relative'>
-              <img className="absolute w-60 md:w-80 mx-auto" id='circlet' src={bg} alt="" />
-              <img className="w-48 md:w-60 lg:mt-2 xl:mt-4 absolute mx-auto" id="gbrPoke" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`} alt={data.name} />
+              <img className="absolute w-3/4 md:w-3/4 lg:w-1/2 mx-auto" id='circlet' src={bg} alt="" />
+              <img className="w-1/2 mt-2 lg:w-1/3 xl:mt-4 absolute mx-auto" id="gbrPoke" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`} alt={data.name} />
             </div>
             <div className="flex">
               <p className='abilities px-3 py-1 text-white rounded-3xl'>ID: #00{data.id}</p>
             </div>
-            <h2 className="text-2xl mt-48 md:mt-28 lg:mt-44 xl:mt-64 text-center font-extrabold capitalize">{data.name}</h2>
+            <h2 className="text-2xl mt-48 xl:mt-72 text-center font-extrabold capitalize">{data.name}</h2>
             <div className="flex justify-around m-2">
               {
                 data.abilities.map(poke => {
--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -45,120 +45,85 @@ const Search = () => {
       setLoading(false);
     }
   };
-    return (
-        <div id="background">
-            <div className="container mx-auto p-4 mt-16 md:mt-24">
-                <Navbar />
-                <div className='bg-white/[.06] backdrop-blur shadow-2xl rounded-md p-4 mt-4 flex justify-around md:h-48  '>
-                    <div className="flex items-center">
-                        <div className=''>
-                            <h1 className="text-3xl mb-3 font-bold text-white text-center md:text-left">Search Your Pokémon</h1>
-                            <h4 className="text-base mb-4 text-white text-center md:text-left">You can search the pokemon by their name</h4>
-                            <form className='relative' onSubmit={handleFormSubmit}>
-                                <input
-                                    type="text"
-                                    className="py-2 px-6 rounded-3xl w-full"
-                                    placeholder="Enter ID/Keyword"
-                                    value={pokemonInput}
-                                    onChange={handleInputChange}
-                                />
-                                <button
-                                    type="submit"
-                                    className="absolute right-0 bg-blue-500 text-white hover:bg-white hover:text-blue-500 font-bold px-4 py-2 rounded-3xl"
-                                >
-                                    Search
-                                </button>
-                            </form>
-                            <p className='mt-2 text-sm text-white text-center md:text-left'>*Use '-' instead of &lt;space&gt;. Type in lowercase*</p>
-                        </div>
-                    </div>
-                    {/* <div className='hidden lg:flex items-center'>
-                        <img className='h-40' src={detective} alt="" />
-                    </div>
-                </div>
-                {loading && <p className='mt-8 text-4xl text-white text-center'>Loading...</p>}
-                {showNoDataMessage && <p className="mt-8 text-4xl text-white text-center">Oops.. No data found</p>}
-                {pokemonData && (
-                    <div className="mt-8 capitalize rounded-md md:flex justify-around bg-white/[.06] backdrop-blur">
-                        <div>
-                            <img className="circletet w-20" src={putih} alt="circle" />
-                            <img
-                                className="gbrPokee h-80 z-30"
-                                src={pokemonData.sprites.other['official-artwork'].front_default}
-                                alt={pokemonData.name}
-                            />
-                        </div>
-                        <div className="h-full flex my-auto">
-                            <div className="card-inner p-4 md:p-0 h-full text-white">
-                                <h2 className="text-2xl font-bold">{pokemonData.name} #00{pokemonData.id}</h2>  
-                                <p>
-                                    <span className="font-bold">Type:</span>{' '}
-                                    {pokemonData.types.map((type) => type.type.name).join(', ')}
-                                </p>
-                                <div className="mt-2">
-                                    {pokemonData.stats.map((stat) => (
-                                        <h3>
-                                            <span className="font-semibold">{stat.stat.name}</span> : {stat.base_stat}
-                                        </h3>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-
+  return (
+    <div id="background">
+      <div className="container mx-auto p-4 mt-16 md:mt-24">
+        <Navbar />
+        <div className='bg-white/[.06] backdrop-blur shadow-2xl rounded-md p-4 mt-4 flex justify-around md:h-48  '>
+          <div className="flex items-center">
+            <div className=''>
+              <h1 className="text-3xl mb-3 font-bold text-white text-center md:text-left">Search Your Pokémon</h1>
+              <h4 className="text-base mb-4 text-white text-center md:text-left">You can search the pokemon by their name</h4>
+              <form className='relative' onSubmit={handleFormSubmit}>
+                <input
+                  type="text"
+                  className="py-2 px-6 rounded-3xl w-full"
+                  placeholder="Enter ID/Keyword"
+                  value={pokemonInput}
+                  onChange={handleInputChange}
+                />
+                <button
+                  type="submit"
+                  className="absolute right-0 bg-blue-500 text-white hover:bg-white hover:text-blue-500 font-bold px-4 py-2 rounded-3xl"
+                >
+                  Search
+                </button>
+              </form>
+              <p className='mt-2 text-sm text-white text-center md:text-left'>*Use '-' instead of &lt;space&gt;. Type in lowercase*</p>
             </div>
-          </div> */}
-           <div className='hidden lg:flex items-center'>
-             <img className='h-40' src={detective} alt="" />
-           </div>
-         </div>
-         {loading &&
-           <div className='mt-8 flex justify-center items-center'>
-             <img id='pokeload' className='w-20 mr-2 rounded-full animate-spin' src={pokeball2} alt="" />
-             <p className='textload text-4xl font-bold text-black text-center ml-2'>Loading...</p>
-           </div>
-         }
-         {showNoDataMessage &&
-           <div className='mt-24'>
-             <div className='flex justify-center'>
-               <p className='textload text-7xl font-bold'>4</p>
-               <img id='pokebob' className='w-20 mx-2 rounded-full animate-bounce' src={pokeball2} alt="" />
-               <p className='textload text-7xl font-bold'>4</p>
-             </div>
-             <p className='textload text-4xl font-bold text-black text-center ml-2'>Error: data not found</p>
-           </div>
-         }
-         {pokemonData && (
-           <div className="mt-8 capitalize rounded-md md:flex justify-around bg-white/[.06] backdrop-blur">
-           <div className='flex items-center ml-4'>
-               <img className="circletet w-20" src={putih} alt="circle" />
-               <img
-                   className="gbrPokee h-80 z-30"
-                   src={pokemonData.sprites.other['official-artwork'].front_default}
-                   alt={pokemonData.name}
-               />
-           </div>
-             <div className="h-full flex my-auto">
-               <div className="card-inner p-4 md:p-0 h-full text-white">
-               <h2 className="text-2xl font-bold">{pokemonData.name} #00{pokemonData.id}</h2>  
-                 <p>
-                   <span className="font-bold">Type:</span>{' '}
-                   {pokemonData.types.map((type) => type.type.name).join(', ')}
-                 </p>
-                 <div className="mt-2">
-                   {pokemonData.stats.map((stat) => (
-                     <h3>
-                       <span className="font-semibold">{stat.stat.name}</span> : {stat.base_stat}
-                     </h3>
-                   ))}
-                 </div>
-               </div>
-             </div>
-           </div>
-         )}
-         </div>
-       </div>
+          </div>
+          <div className='hidden lg:flex items-center'>
+            <img className='h-40' src={detective} alt="" />
+          </div>
+        </div>
+        {loading &&
+          <div className='mt-8 flex justify-center items-center'>
+            <img id='pokeload' className='w-20 mr-2 rounded-full animate-spin' src={pokeball2} alt="" />
+            <p className='textload text-4xl font-bold text-black text-center ml-2'>Loading...</p>
+          </div>
+        }
+        {showNoDataMessage &&
+          <div className='mt-24'>
+            <div className='flex justify-center'>
+              <p className='textload text-7xl font-bold'>4</p>
+              <img id='pokebob' className='w-20 mx-2 rounded-full animate-bounce' src={pokeball2} alt="" />
+              <p className='textload text-7xl font-bold'>4</p>
+            </div>
+            <p className='textload text-4xl font-bold text-black text-center ml-2'>Error: data not found</p>
+          </div>
+        }
+        {pokemonData && (
+          <div className="mt-4 p-4 pt-0 md:pt-4 capitalize w-full rounded-md grid !items-start !max-w-none gap-4 bg-white/[.06] backdrop-blur text-white">
+            <div className='relative col-span-1'>
+              <img id='circlet' className="absolute w-1/2 mx-auto" src={putih} alt="" />
+              <img
+                id='gbrPoke'
+                className="w-1/3 mx-auto md:mt-2"
+                src={pokemonData.sprites.other['official-artwork'].front_default}
+                alt={pokemonData.name}
+              />
+            </div>
+            <div>
+              <p>#00{pokemonData.id}</p>
+              <div className="card-inner md:p-0 h-full">
+                <h2 className="text-2xl font-bold">{pokemonData.name}</h2>
+                <p>
+                  <span className="font-bold">Type:</span>{' '}
+                  {pokemonData.types.map((type) => type.type.name).join(', ')}
+                </p>
+                <div className="mt-2">
+                  {pokemonData.stats.map((stat) => (
+                    <h3>
+                      <span className="font-semibold">{stat.stat.name}</span> : {stat.base_stat}
+                    </h3>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
     //  </div >
   );
 };
